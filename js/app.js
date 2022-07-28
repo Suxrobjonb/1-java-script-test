241ca4801a439e655d8b55728e5f05ddfc7dfe99
Suxrobjonb <suxrobjonb6@gmail.com>
2022-07-28 18:44:40 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var firstName = prompt("Ismingiz");
+console.log (lastName = prompt ("Familyangiz"));
+console.log(birthDate = prompt ("Tug'ilgan kuningiz"));
+console.log(email = prompt ("emailingizni kirirting:"));
+console.log(address = prompt ( "Manzilingiz"));
+console.log(userNum = prompt("telefon raqamingiz"));
+console.log(workPlace = prompt ("Ish joyingiz"));
+console.log(profession = prompt ("kasbingiz"));
+console.log(wather = prompt ("bugungi ob havo?"));
+console.log(game = prompt("Yaxshi ko'rgan o'yingiz"));
+alert("siz botmasmisiz")
+console.log(prompt ("xozir qaysi oy va kun"));
+text = alert ("Tashakur");
